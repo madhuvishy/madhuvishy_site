e8c2b2cb0a5797f4fc18930d8ae51bef0dd98326
Madhumitha Viswanathan <madhuvishy@gmail.com>
2014-08-18 23:43:38 -0400

    Added google analytics tracking

--- conf.py
+++ conf.py
@@ -279,7 +279,7 @@ REDIRECTIONS = [["2014/02/week-2-at-hacker-school-learning/index.html", "/posts/
 # And then do a backup, or run `nikola ping` from the `ping`
 # plugin (`nikola install_plugin ping`).
 # To do manual deployment, set it to []
-# DEPLOY_COMMANDS = []
+DEPLOY_COMMANDS = ["rsync -r output/* ssngeek@ssngeek.webfactional.com:/home/ssngeek/webapps/madhuvishy_in/"]
 
 # For user.github.io/organization.github.io pages, the DEPLOY branch
 # MUST be 'master', and 'gh-pages' for other repositories.
@@ -659,7 +659,7 @@ COMMENT_SYSTEM_ID = "madhuvishy"
 # Google Analytics or whatever else you use. Added to the bottom of <body>
 # in the default template (base.tmpl).
 # (translatable)
-# BODY_END = ""
+BODY_END = "<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga'); ga('create', 'UA-53956915-1', 'auto'); ga('send', 'pageview'); </script>"
 
 # The possibility to extract metadata from the filename by using a
 # regular expression.
